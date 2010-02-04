32df384190ac6cc7a3f6825748fe5dd413514b05
Vesa Marttila <vesa.marttila@mysema.com>
2010-02-04 11:17:44 +0000

    B-01412 : Read-only view shows lemma, description, subtextsources and term.meaning. Lemma is now italic in read-only view and annotate view. Modifying these "new" fields is possible in the annotate view. Added localization accordingly. Also I widened the right container.

--- src/main/java/fi/finlit/edith/ui/pages/document/ViewPage.java
+++ src/main/java/fi/finlit/edith/ui/pages/document/ViewPage.java
@@ -1,17 +1,13 @@
 /*
  * Copyright (c) 2009 Mysema Ltd.
  * All rights reserved.
- * 
+ *
  */
 package fi.finlit.edith.ui.pages.document;
 
 import java.util.List;
 
-import org.apache.tapestry5.Block;
-import org.apache.tapestry5.EventContext;
-import org.apache.tapestry5.annotations.IncludeJavaScriptLibrary;
 import org.apache.tapestry5.annotations.IncludeStylesheet;
-import org.apache.tapestry5.annotations.Log;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
@@ -24,22 +20,41 @@ import fi.finlit.edith.domain.NoteRevisionRepository;
  * @author tiwe
  * @version $Id$
  */
-//@IncludeJavaScriptLibrary( { "classpath:jquery-1.3.2.js", "classpath:TapestryExt.js", "ViewPage.js" })
+// @IncludeJavaScriptLibrary( { "classpath:jquery-1.3.2.js", "classpath:TapestryExt.js",
+// "ViewPage.js" })
 @IncludeStylesheet("context:styles/tei.css")
 @SuppressWarnings("unused")
-public class ViewPage extends AbstractDocumentPage{
-    
+public class ViewPage extends AbstractDocumentPage {
+
     @Inject
     private NoteRevisionRepository noteRevisionRepo;
-    
+
     @Property
     private NoteRevision note;
 
     @Property
     private List<NoteRevision> docNotes;
-    
+
     void setupRender() {
         docNotes = noteRevisionRepo.getOfDocument(getDocumentRevision());
     }
-    
+
+    public String getNoteMetadata() {
+        StringBuilder sb = new StringBuilder();
+        if (note.getDescription() != null) {
+            sb.append(", ");
+            sb.append(note.getDescription());
+        }
+        if (note.getSubtextSources() != null) {
+            sb.append(", ");
+            sb.append(note.getSubtextSources());
+        }
+        if (note.getRevisionOf().getTerm() != null
+                && note.getRevisionOf().getTerm().getMeaning() != null) {
+            sb.append(", ");
+            sb.append(note.getRevisionOf().getTerm().getMeaning());
+        }
+        return sb.toString();
+    }
+
 }
