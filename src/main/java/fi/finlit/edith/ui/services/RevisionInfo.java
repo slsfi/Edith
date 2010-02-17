fc83d884da4f9d58c2c18741d619445eafed03a1
Vesa Marttila <vesa.marttila@mysema.com>
2010-02-17 11:24:03 +0000

    Added equals and hashCode for RevisionInfo and created a test for them as well.

--- src/main/java/fi/finlit/edith/ui/services/RevisionInfo.java
+++ src/main/java/fi/finlit/edith/ui/services/RevisionInfo.java
@@ -10,11 +10,11 @@ package fi.finlit.edith.ui.services;
 public class RevisionInfo {
 
     private final long svnRevision;
-    
+
     private final String created;
-    
+
     private final String creator;
-    
+
     public RevisionInfo(long svnRevision, String created, String creator){
         this.svnRevision = svnRevision;
         this.created = created;
@@ -32,7 +32,30 @@ public class RevisionInfo {
     public String getCreator() {
         return creator;
     }
-    
-    
-    
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + (int) (svnRevision ^ (svnRevision >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        RevisionInfo other = (RevisionInfo) obj;
+        if (svnRevision != other.svnRevision) {
+            return false;
+        }
+        return true;
+    }
 }
---
+++ src/test/java/fi/finlit/edith/ui/services/RevisionInfoTest.java
@@ -0,0 +1,29 @@
+package fi.finlit.edith.ui.services;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class RevisionInfoTest {
+    private RevisionInfo ri;
+    private RevisionInfo ri2;
+
+    @Before
+    public void setUp() {
+        ri = new RevisionInfo(100, "today", "somebody");
+        ri2 = new RevisionInfo(100, "yesterday", "somebody else");
+    }
+
+    @Test
+    public void testEquals() {
+        assertTrue(ri.equals(ri2));
+    }
+
+    @Test
+    public void testHashCode() {
+        assertEquals(ri.hashCode(), ri2.hashCode());
+    }
+}
