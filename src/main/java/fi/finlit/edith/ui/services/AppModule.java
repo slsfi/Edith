a770ffcfd0372d748548ca2f1cd5701e8204fcc4
Vesa Marttila <vesa.marttila@mysema.com>
2010-02-22 15:33:08 +0000

    Updated project version 0.1.1 -> 0.1.2.

--- src/main/java/fi/finlit/edith/ui/services/AppModule.java
+++ src/main/java/fi/finlit/edith/ui/services/AppModule.java
@@ -40,7 +40,7 @@ public class AppModule {
         // general config
         configuration.add(SymbolConstants.SUPPORTED_LOCALES, "fi,en,sv,de");
         configuration.add(SymbolConstants.PRODUCTION_MODE, System.getProperty("production.mode", "false"));
-        configuration.add(SymbolConstants.APPLICATION_VERSION, "0.1.1");
+        configuration.add(SymbolConstants.APPLICATION_VERSION, "0.1.2");
 
         // Spring Security config
         configuration.add("spring-security.loginform.url", "/login");
