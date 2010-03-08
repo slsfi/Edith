0414f3f8dd3fe9a376c2da5ac8b14bde5a7fedf8
Vesa Marttila <vesa.marttila@mysema.com>
2010-03-08 13:21:46 +0000

    Removed redundant throws in UserDetailsServiceImpl and redundant else-block.

--- src/main/java/fi/finlit/edith/ui/services/UserDetailsServiceImpl.java
+++ src/main/java/fi/finlit/edith/ui/services/UserDetailsServiceImpl.java
@@ -1,12 +1,11 @@
 /*
  * Copyright (c) 2009 Mysema Ltd.
  * All rights reserved.
- * 
+ *
  */
 package fi.finlit.edith.ui.services;
 
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.springframework.dao.DataAccessException;
 import org.springframework.security.userdetails.UserDetails;
 import org.springframework.security.userdetails.UserDetailsService;
 import org.springframework.security.userdetails.UsernameNotFoundException;
@@ -21,22 +20,19 @@ import fi.finlit.edith.domain.UserRepository;
  * @version $Id$
  */
 public class UserDetailsServiceImpl implements UserDetailsService{
-    
+
     @Inject
     private UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException, DataAccessException {
+    public UserDetails loadUserByUsername(String username) {
         User user = userRepository.getByUsername(username);
         if (user != null){
             return new UserDetailsImpl(
-                    user.getUsername(), user.getPassword(), 
-                    user.getProfile().getAuthorities());    
-        }else{
-            throw new UsernameNotFoundException("User " + username + " not found");
+                    user.getUsername(), user.getPassword(),
+                    user.getProfile().getAuthorities());
         }
-        
+        throw new UsernameNotFoundException("User " + username + " not found");
     }
 
 }
