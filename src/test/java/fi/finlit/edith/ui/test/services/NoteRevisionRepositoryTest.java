799b91ef405a92925ab9f64c09e79c4aa1bc7d96
Vesa Marttila <vesa.marttila@mysema.com>
2010-03-12 14:51:15 +0000

    Created new test NoteRevisionRepositoryTest.queryNotes_sorting_is_case_insensitive() which confirms a bug that lemmas are sorted case sensitively instead of case insensitively (e.g. "B" < "a" == true). This is a bug in RDFBean.

--- src/test/java/fi/finlit/edith/ui/test/services/NoteRevisionRepositoryTest.java
+++ src/test/java/fi/finlit/edith/ui/test/services/NoteRevisionRepositoryTest.java
@@ -5,13 +5,23 @@
  */
 package fi.finlit.edith.ui.test.services;
 
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.tapestry5.PropertyConduit;
+import org.apache.tapestry5.beaneditor.BeanModel;
+import org.apache.tapestry5.beaneditor.PropertyModel;
+import org.apache.tapestry5.grid.ColumnSort;
+import org.apache.tapestry5.grid.GridDataSource;
+import org.apache.tapestry5.grid.SortConstraint;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.junit.Before;
@@ -26,6 +36,7 @@ import fi.finlit.edith.domain.NoteRevision;
 import fi.finlit.edith.domain.NoteRevisionRepository;
 import fi.finlit.edith.ui.services.AdminService;
 import fi.finlit.edith.ui.services.svn.RevisionInfo;
+
 
 /**
  * NoteRevisionRepositoryTest provides
@@ -82,6 +93,99 @@ public class NoteRevisionRepositoryTest extends AbstractServiceTest{
     @Test
     public void queryNotes(){
         assertTrue(noteRevisionRepo.queryNotes("annoit").getAvailableRows() > 0);
+    }
+
+    @Test
+    public void queryNotes_sorting_is_case_insensitive() {
+        noteRepo.createNote(docRev, "5", "a");
+        noteRepo.createNote(docRev, "6", "b");
+        noteRepo.createNote(docRev, "7", "A");
+        noteRepo.createNote(docRev, "8", "B");
+        GridDataSource gds = noteRevisionRepo.queryNotes("*");
+        int n = gds.getAvailableRows();
+        List<SortConstraint> sortConstraints = new ArrayList<SortConstraint>();
+        sortConstraints.add(new SortConstraint(new PropertyModelMock(), ColumnSort.ASCENDING));
+        gds.prepare(0, 100, sortConstraints);
+        String previous = null;
+        for (int i = 0; i < n; ++i) {
+            String current = gds.getRowValue(i).toString().toLowerCase();
+            if (previous != null) {
+                assertThat(previous, lessThanOrEqualTo(current));
+            }
+            previous = current;
+        }
+    }
+
+    private static final class PropertyModelMock implements PropertyModel {
+
+        @Override
+        public PropertyModel dataType(String dataType) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public PropertyConduit getConduit() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public String getDataType() {
+            return null;
+        }
+
+        @Override
+        public String getId() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public String getLabel() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public String getPropertyName() {
+            return "lemma";
+        }
+
+        @Override
+        public Class getPropertyType() {
+            return NoteRevision.class;
+        }
+
+        @Override
+        public boolean isSortable() {
+            return true;
+        }
+
+        @Override
+        public PropertyModel label(String label) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public BeanModel model() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public PropertyModel sortable(boolean sortable) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
     }
 
     @Test
