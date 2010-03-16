c4fc702dd0c84bd0bf51340e3c2cf954fec9c870
Vesa Marttila <vesa.marttila@mysema.com>
2010-03-16 08:54:23 +0000

    Working term autocompletion.

--- src/main/java/fi/finlit/edith/ui/mixins/term-autocomplete.js
+++ src/main/java/fi/finlit/edith/ui/mixins/term-autocomplete.js
@@ -3,20 +3,9 @@ jQuery.noConflict();
 
 Tapestry.Initializer.termAutocompleter = function(elementId) {
 	jQuery("#" + elementId).autocomplete({
-//		open: function(event, ui) {
-//			alert(ui.item.basicForm);
-//		},
 		select: function(event, ui) { 
-//			alert(ui.item.basicForm);
-//			alert(ui.item.meaning);
-			jQuery("input[]").text(ui.item.meaning);
-			alert(ui.item.meaning);
-			// TODO LIVE?
+			jQuery("textarea[name='meaning']").attr("value", ui.item.meaning);
 		}
 		
 	});
 }
-
-//var update_term_autocomplete = function(item) {
-//	Tapestry.Logging.info("Got: " + item);
-//}
