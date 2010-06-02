b0398f7a74dd7a908b18b57ebf7e7d87e06a42f4
Vesa Marttila <vesa.marttila@mysema.com>
2010-06-02 14:32:07 +0000

    Fixed FindBugs' @SuppressWarnings, of course it isn't recursive, silly me.

--- src/main/java/fi/finlit/edith/ui/components/package-info.java
+++ src/main/java/fi/finlit/edith/ui/components/package-info.java
@@ -1,5 +1,5 @@
 @SuppressWarnings("UUF_UNUSED_FIELD")
-package fi.finlit.edith.ui;
+package fi.finlit.edith.ui.components;
 
 import edu.umd.cs.findbugs.annotations.SuppressWarnings;
 
---
+++ src/main/java/fi/finlit/edith/ui/pages/package-info.java
@@ -0,0 +1,5 @@
+@SuppressWarnings( { "UUF_UNUSED_FIELD", "URF_UNREAD_FIELD" })
+package fi.finlit.edith.ui.pages;
+
+import edu.umd.cs.findbugs.annotations.SuppressWarnings;
+
