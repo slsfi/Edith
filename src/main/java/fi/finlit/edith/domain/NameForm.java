5b2987ec63474f77bffe7419646977b00c177ab1
Vesa Marttila <vesa.marttila@mysema.com>
2010-08-26 07:58:19 +0000

    Added a test for NameForm.getName().

--- src/main/java/fi/finlit/edith/domain/NameForm.java
+++ src/main/java/fi/finlit/edith/domain/NameForm.java
@@ -73,36 +73,6 @@ public class NameForm extends Identifiable {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (getId() == null ? 0 : getId().hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        NameForm other = (NameForm) obj;
-        if (getId() == null) {
-            if (other.getId() != null) {
-                return false;
-            }
-        } else if (!getId().equals(other.getId())) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
     public String toString() {
         return "NameForm [description=" + description + ", name=" + getName() + "]";
     }
