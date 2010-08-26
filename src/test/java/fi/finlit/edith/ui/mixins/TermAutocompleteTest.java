988c9917398466174a9b104bd5ca4cb60deffe42
Vesa Marttila <vesa.marttila@mysema.com>
2010-08-26 08:32:08 +0000

    Added test for TermAutocomplete.

---
+++ src/test/java/fi/finlit/edith/ui/mixins/TermAutocompleteTest.java
@@ -0,0 +1,55 @@
+package fi.finlit.edith.ui.mixins;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.tapestry5.json.JSONArray;
+import org.apache.tapestry5.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import fi.finlit.edith.domain.Term;
+import fi.finlit.edith.domain.TermLanguage;
+
+public class TermAutocompleteTest {
+    private TermAutocomplete autocomplete;
+
+    @Before
+    public void Before() {
+        autocomplete = new TermAutocomplete();
+    }
+
+    @Test
+    public void Generate_Response_One_Element_Found_And_Meaning_Is_Set() {
+        List<Object> terms = new ArrayList<Object>();
+        Term term = new Term();
+        term.setBasicForm("kuusi");
+        term.setLanguage(TermLanguage.FINNISH);
+        term.setMeaning("Tarkoittaa puuta. Eli ei numeroa, vaan puuta.");
+        terms.add(term);
+        JSONArray response = autocomplete.generateResponse(terms);
+        JSONObject o = (JSONObject) response.get(0);
+        assertEquals(term.getBasicForm(), o.get("basicForm"));
+        assertEquals(term.getMeaning(), o.get("meaning"));
+        assertEquals(String.valueOf(term.getLanguage()), o.get("language"));
+        assertEquals(term.getBasicForm() + " - " + StringUtils.abbreviate(term.getMeaning(), 32),
+                o.get("value"));
+    }
+
+    @Test
+    public void Generate_Response_One_Element_Found_And_Meaning_Is_Null() {
+        List<Object> terms = new ArrayList<Object>();
+        Term term = new Term();
+        term.setBasicForm("kuusi");
+        term.setLanguage(TermLanguage.FINNISH);
+        terms.add(term);
+        JSONArray response = autocomplete.generateResponse(terms);
+        JSONObject o = (JSONObject) response.get(0);
+        assertEquals(term.getBasicForm(), o.get("basicForm"));
+        assertEquals(String.valueOf(term.getLanguage()), o.get("language"));
+        assertEquals(term.getBasicForm(), o.get("value"));
+    }
+}
