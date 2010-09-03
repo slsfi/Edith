7a709a98a5ab0a38997fa62658a19293ba3c3718
Vesa Marttila <vesa.marttila@mysema.com>
2010-09-03 07:59:30 +0000

    Added test for JQueryAutocomplete.

---
+++ src/test/java/fi/finlit/edith/ui/mixins/JQueryAutocompleteTest.java
@@ -0,0 +1,22 @@
+package fi.finlit.edith.ui.mixins;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.tapestry5.json.JSONArray;
+import org.junit.Test;
+
+public class JQueryAutocompleteTest {
+    private JQueryAutocomplete autocomplete;
+
+    @Test
+    public void testGenerateResponse() {
+        autocomplete = new JQueryAutocomplete();
+        List<Object> matches = new ArrayList<Object>();
+        matches.add("foobar");
+        JSONArray result = autocomplete.generateResponse(matches);
+        assertEquals("foobar", result.get(0));
+    }
+}
