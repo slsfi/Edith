1da95954e45aec460bda3e07320bd5aebc6a412b
Vesa Marttila <vesa.marttila@mysema.com>
2010-10-20 14:48:55 +0000

    Index now forwards to DocumentsPage.

--- src/main/java/fi/finlit/edith/ui/pages/Index.java
+++ src/main/java/fi/finlit/edith/ui/pages/Index.java
@@ -8,6 +8,7 @@ package fi.finlit.edith.ui.pages;
 import java.io.IOException;
 
 import org.apache.tapestry5.EventContext;
+import org.apache.tapestry5.annotations.InjectPage;
 import org.apache.tapestry5.ioc.annotations.Inject;
 import org.apache.tapestry5.services.Response;
 
@@ -22,10 +23,14 @@ public class Index {
     @Inject
     private Response response;
 
-    void onActivate(EventContext eventContext) throws IOException {
+    @InjectPage
+    private DocumentsPage documentsPage;
+
+    Object onActivate(EventContext eventContext) throws IOException {
         if (eventContext.getCount() > 0) {
             response.sendError(HttpError.PAGE_NOT_FOUND, "Page not found!");
         }
+        return documentsPage;
     }
 
 }
