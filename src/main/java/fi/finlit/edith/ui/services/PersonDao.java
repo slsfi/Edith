07578879be82921d6a636139ae37e11f3c29973e
Vesa Marttila <vesa.marttila@mysema.com>
2011-06-17 09:09:16 +0000

    PersonDao and PlaceDao stubs.

---
+++ src/main/java/fi/finlit/edith/ui/services/PersonDao.java
@@ -0,0 +1,17 @@
+package fi.finlit.edith.ui.services;
+
+import java.util.Collection;
+
+import fi.finlit.edith.sql.domain.Person;
+
+public interface PersonDao extends Dao<Person, Long> {
+
+    Collection<Person> findByStartOfFirstAndLastName(String partial, int limit);
+
+    void remove(String personId);
+
+    void remove(Person person);
+
+    void save(Person person);
+
+}
---
+++ src/main/java/fi/finlit/edith/ui/services/PlaceDao.java
@@ -0,0 +1,17 @@
+package fi.finlit.edith.ui.services;
+
+import java.util.Collection;
+
+import fi.finlit.edith.sql.domain.Place;
+
+public interface PlaceDao extends Dao<Place, Long> {
+
+    Collection<Place> findByStartOfName(String partial, int limit);
+
+    void remove(String placeId);
+
+    void save(Place place);
+
+    void remove(Place place);
+
+}
