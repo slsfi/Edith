74293852c26f6249a2edd76c9d7ecb2c0bedc094
Marko Lavikainen <marko.lavikainen@mysema.com>
2011-08-25 07:52:20 +0000

    Made hacky "fix" to convert event links to buttons

--- src/main/java/fi/finlit/edith/ui/components/note/NoteForm.js
+++ src/main/java/fi/finlit/edith/ui/components/note/NoteForm.js
@@ -1,11 +1,13 @@
-
 var SLSNoteForm = {
-	toggleOtherLanguage : function() {
+    toggleOtherLanguage : function() {
       if (jQuery("select[name='language']").val() === "OTHER") {
           jQuery(":input[name='otherLanguage']").show();
       } else {
           jQuery(":input[name='otherLanguage']").hide();
       }
+    },
+    triggerNextActionLink : function(element) {
+      var actionLink = jQuery(element).next('span').find('a').click();
+      Tapestry.findZoneManager(actionLink.get(0)).updateFromURL(actionLink.attr("href"));
     }
-    
 };
