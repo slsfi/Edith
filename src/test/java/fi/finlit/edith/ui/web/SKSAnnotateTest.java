6faf837d8d063d043568323acb22cd8b7e560a88
Vesa Marttila <vesa.marttila@mysema.com>
2011-09-14 11:51:09 +0000

    Web tests now run by default in Maven.
    Upgraded Selenium to 2.5 (now works with Firefox 6).
    
    Selenium upgrade forced to remove RemoteWebElement.toggle call in SKSAnnotateTest,  .click supposedly works as well, but there were no assertions in the test so hard to verify.

--- src/test/java/fi/finlit/edith/ui/web/SKSAnnotateTest.java
+++ src/test/java/fi/finlit/edith/ui/web/SKSAnnotateTest.java
@@ -13,49 +13,49 @@ import fi.finlit.edith.Setups;
 //TODO Fix SKS to new UI
 @Ignore
 public class SKSAnnotateTest extends AbstractSeleniumTest {
-    
+
     @Test
     public void GetForm_FillForm_And_Submit() {
         login("lassi", "lassi");
         get("/addnote/12/play-act-sp53-p/play-act-sp53-p/minusta");
         get("/document/annotate/12"); // nummisuutarit_simp.xml
-         
+
         // get form
         findElement(By.cssSelector("a.notelink")).click();
-        By noteFormSubmit = By.cssSelector("input.noteFormSubmit"); 
-        wait(element(noteFormSubmit));        
-        
+        By noteFormSubmit = By.cssSelector("input.noteFormSubmit");
+        wait(element(noteFormSubmit));
+
         // fill form
         String random = UUID.randomUUID().toString();
         findElement(By.cssSelector("div.lemma input")).sendKeys("lemma-"+random);
         findElement(By.cssSelector("div.lemmaMeaning textarea")).sendKeys("lemmaMeaning-"+random);
 //        findElement(By.cssSelector("div.basicForm input")).sendKeys("basicForm-"+random);
 //        findElement(By.cssSelector("div.basicFormMeaning input")).sendKeys("basicFormMeaning-"+random);
-        
+
         // submit
         findElement(noteFormSubmit).click();
-        
+
     }
-    
+
     @Test
     public void Submit_NoteForm_As_New() {
         login("lassi", "lassi");
         get("/addnote/12/play-act-sp53-p/play-act-sp53-p/minusta");
         get("/document/annotate/12"); // nummisuutarit_simp.xml
-         
+
         // get form
         findElement(By.cssSelector("a.notelink")).click();
-        By noteFormSubmit = By.cssSelector("input.noteFormSubmit"); 
+        By noteFormSubmit = By.cssSelector("input.noteFormSubmit");
         wait(element(noteFormSubmit));
-        
+
         // fill form
-        findElement(By.cssSelector(".saveAsNew input")).toggle();
-        
+        findElement(By.cssSelector(".saveAsNew input")).click();
+
         // submit
         findElement(noteFormSubmit).click();
-        
+
     }
-    
+
     @Override
     public WebappStarter starter() {
         return Setups.SKS_TEST;
