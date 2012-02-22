f91644b92b19b7b81e669c63a7d035c1f928573c
Timo Westkämper <timo.westkamper@mysema.com>
2012-02-22 17:42:21 +0200

    #27 began working on print view

--- src/main/java/fi/finlit/edith/ui/pages/document/Print.java
+++ src/main/java/fi/finlit/edith/ui/pages/document/Print.java
@@ -5,13 +5,34 @@
  */
 package fi.finlit.edith.ui.pages.document;
 
+import java.util.List;
+
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import fi.finlit.edith.sql.domain.DocumentNote;
+import fi.finlit.edith.ui.services.DocumentNoteDao;
 
 @Import(stylesheet={
     "context:styles/base.css",
     "context:styles/edith.css",
     "context:styles/tei.css"
 })
+@SuppressWarnings("unused")
 public class Print extends AbstractDocumentPage{
+    
+    @Inject
+    private DocumentNoteDao documentNoteRepository;
+
+    @Property
+    private DocumentNote documentNote;
+
+    @Property
+    private List<DocumentNote> documentNotes;
+
+    void setupRender() {
+        documentNotes = documentNoteRepository.getOfDocument(getDocument());
+    }
 
 }
