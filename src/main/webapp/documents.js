e7474f2886e1295fcbb452f7d8b5a3fb7fbf5178
Lassi Immonen <lassi.immonen@mysema.com>
2013-03-13 16:25:07 +0200

    Add basic grid to documents.html

--- src/main/webapp/documents.js
+++ src/main/webapp/documents.js
@@ -4,7 +4,7 @@ require([], function() {
   require(['jquery', 'underscore', 'backbone', 'js/views/documents',
            'js/views/document', 'text!templates/header.html'],
           function($, _, Backbone, DocumentsView, DocumentView, headerTemplate) {
-    $('body').prepend(headerTemplate);
+    $('#header').append(headerTemplate);
     new DocumentsView({el: $('#documents')});
     new DocumentView({el: $('#document')});
   });   
