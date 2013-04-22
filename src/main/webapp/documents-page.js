af3f49019784ba97816c49247d845f82dbe514cb
Vesa Marttila <vtmarttila@gmail.com>
2013-04-22 12:08:23 +0300

    Add router for documents
    While at it refactor frontend directory structure

--- src/main/webapp/documents-page.js
+++ src/main/webapp/documents-page.js
@@ -1,17 +1,47 @@
 require.config(window.rconfig);
 
 require([], function() {
-  require(['jquery', 'underscore', 'backbone', 'js/vent', 'js/views/documents',
-           'js/views/document', 'js/views/comments', 'text!templates/header.html'],
-          function($, _, Backbone, vent, DocumentsView, DocumentView, CommentsView, headerTemplate) {
+  require(['jquery', 'underscore', 'backbone', 'js/vent',
+           'js/views/documents-page/document', 'js/views/documents-page/listing',
+           'text!templates/header.html'],
+          function($, _, Backbone, vent, DocumentView, ListingView, headerTemplate) {
     $('#header').append(headerTemplate);
-    var documents = new DocumentsView({el: $('#documents')});
-    var comments = new CommentsView({el: $('#comments')});
-    var document = new DocumentView({el: $('#document')});
-    vent.on('document:open', function() {
-      documents.$el.hide();
-      comments.$el.hide();
-      document.$el.show();
+    
+    var DocumentsRouter = Backbone.Router.extend({
+      views: {},
+      
+      initialize: function() {
+        _.bindAll(this);
+        this.views = {listing: new ListingView({el: $('#listing')}),
+                      document: new DocumentView({el: $('#document')})};
+        var self = this;
+        vent.on('route:change', function(path) {
+          self.navigate(path, {trigger: true})
+        });
+      },
+      
+      routes: {'': 'listing',
+              'documents/:id': 'showDocument'},
+
+      hideAll: function() {
+        _(this.views).each(function(view) {
+          view.$el.hide();
+        }); 
+      },
+              
+      listing: function() {
+        this.hideAll();
+        this.views.listing.$el.show();
+      },
+      
+      showDocument: function(id) {          
+        this.hideAll();
+        vent.trigger('document:open', id);
+        this.views.document.$el.show();
+      }
     });
+    
+    var router = new DocumentsRouter();
+    Backbone.history.start();
   });
 });
