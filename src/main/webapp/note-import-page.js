bbf2f1b76f190b6be19feb974c85a2fd23d7e883
Timo Westkämper <timo.westkamper@mysema.com>
2013-05-17 08:16:10 +0300

    Ajax form upload

--- src/main/webapp/note-import-page.js
+++ src/main/webapp/note-import-page.js
@@ -4,5 +4,30 @@ require([], function() {
   require(['jquery', 'underscore', 'backbone', 'text!templates/header.html'],
           function($, _, Backbone, headerTemplate) {
     $('body').prepend(headerTemplate);
+    
+    var NoteImport = Backbone.View.extend({
+      events: {'submit #import': 'submit'},
+      
+      initialize: function() {
+        _.bindAll(this);
+      },
+      
+      submit: function() {
+        var self = this;        
+        var formData = new FormData(this.$("#import").get(0));
+        $.ajax('api/notes/import',
+            {type: 'post', 
+             processData: false,
+             contentType: false,
+             data: formData,
+             success: function(data) {
+               //console.log(data);
+               self.$(".alert").html("Imported " + data + " notes").show();
+             }});
+        return false;
+      }
+    });
+    
+    new NoteImport({el: $('body')});
   });
 });
