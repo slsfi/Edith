c184cddeed4e4c7bf159ed4bc45cf279d5b41b6b
Vesa Marttila <vtmarttila@gmail.com>
2013-05-17 17:15:20 +0300

    Implement tabs for note edit, search and list

--- src/main/webapp/js/views/workspace/annotator.js
+++ src/main/webapp/js/views/workspace/annotator.js
@@ -1,20 +1,40 @@
-define(['jquery', 'underscore', 'backbone', 'vent',
+define(['jquery', 'underscore', 'backbone', 'vent', 'bootstrap',
         'text!/templates/workspace/annotator.html',
         'views/workspace/annotator/document',
         'views/workspace/annotator/note-list'],
-  function($, _, Backbone, vent, template, Document, NoteList) {
+        function($, _, Backbone, vent, Bootstrap, template, Document, NoteList) {
+
+  // TODO: Move into dedicated files and implement
+  var NoteEdit = Backbone.View;
+  var NoteSearch = Backbone.View;
+
   var AnnotatorView = Backbone.View.extend({
+    events: {'shown a[data-toggle="tab"]': 'switchTab'},
+
     initialize: function() {
-      _.bindAll(this, 'render');
+      _.bindAll(this, 'render', 'switchTab');
       this.render();
     },
 
     render: function() {
       this.$el.html(template);
-      this.documentView = new Document({el: this.$('#document')});
-      this.noteListView = new NoteList({el: this.$('#note-list')});
+      new Document({el: this.$('#document')}),
+      this.views = {noteList: new NoteList({el: this.$('#note-list')}),
+                    noteEdit: new NoteEdit({el: this.$('#note-edit')}),
+                    noteSearch: new NoteSearch({el: this.$('#note-search')})};
+    },
+
+    switchTab: function(evt) {
+      var target = evt.target.getAttribute('data-target');
+      _.each(this.views, function(view) {
+                           if (view.el.id === target) {
+                             view.$el.show();
+                           } else {
+                             view.$el.hide();
+                           }
+                         });
     }
-  })
+  });
 
   return AnnotatorView;
 });
