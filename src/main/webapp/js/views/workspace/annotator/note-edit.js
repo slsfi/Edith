463352ac7cdab6ccef6dae10a7e95a39e2718620
Vesa Marttila <vtmarttila@gmail.com>
2013-05-26 23:15:42 +0300

    Clean note edit code

--- src/main/webapp/js/views/workspace/annotator/note-edit.js
+++ src/main/webapp/js/views/workspace/annotator/note-edit.js
@@ -20,44 +20,31 @@ define(['jquery', 'underscore', 'backbone', 'vent', 'handlebars',
   var DocumentNoteForm = Backbone.View.extend({
     template: Handlebars.compile(documentNoteFormTemplate),
 
-    events: {'click #save-document-note': 'saveDocumentNote',
-             'keyup input': 'setDirty'},
+    events: {'keyup input': 'setDirty'},
 
     initialize: function() {
-      _.bindAll(this, 'render', 'saveDocumentNote', 'setDirty', 'update');
+      _.bindAll(this, 'render', 'save', 'setDirty', 'annotate');
       var self = this;
-      vent.on('document-note:open document-note:change',
-              function(documentNote) {
-                // TODO: Hook into 'unload' event
-                if ((self.isDirty || self.hasDirtyChildren) &&
-                    !confirm('U haz unsaved changes, continue?')) {
-                  return;
-                }
-                vent.trigger('note:open', documentNote.note);
-                self.documentNote = documentNote;
-                self.render();
-              });
-      vent.on('note:create', function() {
-        // TODO: Hook into 'unload' event
-        if ((self.isDirty || self.hasDirtyChildren) &&
-            !confirm('U haz unsaved changes, continue?')) {
-          return;
-        }
-        self.documentNote = null;
-        self.$el.empty();
-      });
-      vent.on('note:dirty', function() { self.hasDirtyChildren = true; });
-      vent.on('note:change', function() { self.hasDirtyChildren = false; });
-      vent.on('document:selection', function(documentId, selection) {
-        if (self.$el.is(':visible')) {
-          self.update(documentId, selection)
-        }
-      });
+      vent.on('document-note:change', function(documentNote) {
+                                        self.documentNote = documentNote;
+                                        self.render();
+                                      });
+      this.render();
     },
 
     render: function() {
+      if (!this.documentNote) {
+        return;
+      }
       this.$el.html(this.template(this.documentNote))
               .effect('highlight', {color: 'lightblue'}, 500);
+    },
+
+    open: function(documentNote, options) {
+      var self = this;
+      console.log(documentNote);
+      this.documentNote = documentNote;
+      this.render();
     },
 
     setDirty: function() {
@@ -65,17 +52,11 @@ define(['jquery', 'underscore', 'backbone', 'vent', 'handlebars',
       this.$('#save-document-note').removeAttr('disabled');
     },
     
-    update: function(documentId, selection) {
-      if (!this.documentNote) {
-        this.documentNote = {document: documentId};
-      }
-      this.documentNote.fullSelection = selection.selection;
-      this.render();
-      this.setDirty();
-    },
+    annotate: function() {
+      // TODO: implement
+    }
 
-    saveDocumentNote: function(evt) {
-      evt.preventDefault();
+    save: function() {
       var arr = this.$el.serializeArray();
       var data = _(arr).reduce(function(acc, field) {
                                  acc[field.name] = field.value;
@@ -97,25 +78,11 @@ define(['jquery', 'underscore', 'backbone', 'vent', 'handlebars',
                        vent.trigger('document-note:change', data);
                      }};
       // TODO: Actual attaching to document
-      if (this.documentNote.id) {
-        $.ajax(request);
-      } else if (!this.documentNote.id && this.documentNote.note) {
+      if (!this.documentNote.id) {
         request.url = '/api/document-notes/';
         request.type = 'POST';
-        $.ajax(request);
-      } else {
-        var proceed = confirm('Note will be stored as well?');
-        if (proceed) {
-          this.options.saveNote(function(note) {
-            request.url = '/api/document-notes/';
-            request.type = 'POST';
-            data.note = note.id;
-            request.data = JSON.stringify(data);
-            $.ajax(request);
-          });
-        }
       }
-
+      $.ajax(request);
     },
   });
 
@@ -137,37 +104,26 @@ define(['jquery', 'underscore', 'backbone', 'vent', 'handlebars',
                                                 {name: 'document', items: ['Source']}]};
 
   var NoteForm = Backbone.View.extend({
-    events: {'click #save-note': 'saveNote',
-             'keyup input': 'setDirty',
+    events: {'keyup input': 'setDirty',
              'change input': 'setDirty',
              'change select': 'setDirty'},
   
     template: Handlebars.compile(noteFormTemplate),
 
     initialize: function() {
-      _.bindAll(this, 'render', 'saveNote', 'saveNoteExt', 'setDirty');
+      _.bindAll(this, 'render', 'save', 'setDirty', 'open');
       var self = this;
       vent.on('note:change', function(note) {
                                self.note = note;
                                self.render();
                              });
-      vent.on('note:open', function(noteId) {
-                             if (self.note && (noteId === self.note.id)) {
-                               return;
-                             }
-                             $.getJSON('/api/notes/' + noteId,
-                                       function(note) {
-                                         self.note = note;
-                                         self.render();
-                                       });
-                           });
-      vent.on('note:create', function() {
-                               self.note = {};
-                               self.render();
-                             });
+      this.render();
     },
 
     render: function() {
+      if (!this.note) {
+        return;
+      }
       _(CKEditor.instances).each(function(editor) {
         editor.destroy();
       });
@@ -181,53 +137,17 @@ define(['jquery', 'underscore', 'backbone', 'vent', 'handlebars',
              });
     },
     
+    open: function(note) {
+      this.note = note;
+      this.render();
+    },
+
     setDirty: function() {
+      this.isDirty = true;
       this.$('#save-note').removeAttr('disabled');
-      vent.trigger('note:dirty');
     },
 
-    // FIXME: Dirty copypasting
-    saveNoteExt: function(callback) {
-      var arr = this.$el.serializeArray();
-      var data = _(arr).reduce(function(acc, field) {
-                                 var name = field.name;
-                                 var value = field.value;
-                                 var xs = name.split('.');
-                                 if (xs.length > 2) {
-                                   throw 'Only once nested paths are supported'
-                                 } else if (xs.length === 2) {
-                                   var o = acc[xs[0]] || {};
-                                   o[xs[1]] = value;
-                                   acc[xs[0]] = o;
-                                 } else {
-                                   acc[name] = value;
-                                 }
-                                 return acc;
-                               }, {});
-      var types = _(this.$('input[name="types"]').serializeArray())
-                    .map(function(field) {
-                           return field.value;
-                         });
-      data.types = types;
-      var self = this;
-      var request = {url: '/api/notes/' + this.note.id,
-                     type: 'PUT',
-                     dataType: 'json',
-                     contentType: "application/json; charset=utf-8",
-                     data: JSON.stringify(data),
-                     success: function(data) {
-                       callback(data);
-                       vent.trigger('note:change', data);
-                     }};
-      if (!this.note.id) {
-        request.url = '/api/notes/';
-        request.type = 'POST';
-      }
-      $.ajax(request);
-    },
-
-    saveNote: function(evt) {
-      evt.preventDefault();
+    save: function() {
       var arr = this.$el.serializeArray();
       var data = _(arr).reduce(function(acc, field) {
                                  var name = field.name;
@@ -269,16 +189,81 @@ define(['jquery', 'underscore', 'backbone', 'vent', 'handlebars',
   var NoteEdit = Backbone.View.extend({
     template: Handlebars.compile(noteEditTemplate),
 
+    events: {'click #save-document-note': 'saveDocumentNote',
+             'click #save-note': 'saveNote'},
+
     initialize: function() {
-      _.bindAll(this, 'render');
+      _.bindAll(this, 'render', 'close', 'open', 'create', 'annotate', 'saveDocumentNote', 'saveNote');
       var self = this;
+      vent.on('document:selection', function(documentId, selection) {
+        if (self.$el.is(':visible')) {
+          self.annotate(documentId, selection);
+        }
+      });
+      vent.on('document-note:open', this.open);
       this.render();
     },
 
     render: function() {
       this.$el.html(this.template);
-      var noteForm = new NoteForm({el: this.$('form#note')});
-      new DocumentNoteForm({el: this.$('form#document-note'), saveNote: noteForm.saveNoteExt});
+      this.noteForm = new NoteForm({el: this.$('form#note')});
+      this.documentNoteForm = new DocumentNoteForm({el: this.$('form#document-note')});
+    },
+
+    close: function() {
+      if (this.noteForm.isDirty || this.documentNoteForm.isDirty) {
+        if (!confirm('U haz unsaved changes, continue?')) {
+          return;
+        }
+      }
+      this.noteForm.close();
+      this.documentNoteForm.close();
+    },
+
+    open: function(documentNote) {
+      if (this.noteForm.isDirty || this.documentNoteForm.isDirty) {
+        if (!confirm('U haz unsaved changes, continue?')) {
+          return;
+        }
+      }
+      this.documentNoteForm.open(documentNote);
+      $.getJSON('/api/notes/' + documentNote.note,
+                this.noteForm.open)
+    },
+
+    create: function() {
+      if (this.noteForm.isDirty || this.documentNoteForm.isDirty) {
+        if (!confirm('U haz unsaved changes, continue?')) {
+          return;
+        }
+      }
+      noteForm.open({});
+    },
+
+    annotate: function(documentId, selection) {
+      if (this.documentNoteForm.isDirty) {
+        if (!confirm('U haz unsaved changes, continue?')) {
+          return;
+        }
+      }
+      this.documentNoteForm.open({document: documentId, fullSelection: selection.selection});
+    },
+
+    saveDocumentNote: function(evt) {
+      evt.preventDefault();
+      var self = this;
+      if (this.documentNoteForm.documentNote.note) {
+        this.documentNoteForm.save();
+      } else {
+        noteForm.save({success: function() {
+                                  self.documentNoteForm.save();
+                                }});
+      }
+    },
+
+    saveNote: function(evt) {
+      evt.preventDefault();
+      this.noteForm.save();
     }
   });
 
