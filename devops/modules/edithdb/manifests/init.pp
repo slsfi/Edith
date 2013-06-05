7a15c30083eedaff30dc126ea6767c21a3180a5f
jstaffans <johannes.staffans@gmail.com>
2013-06-05 18:29:55 +0300

    Fixed db user

--- devops/modules/edithdb/manifests/init.pp
+++ devops/modules/edithdb/manifests/init.pp
@@ -14,36 +14,36 @@ class edithdb {
     require => Class['mysql::server'], 
   } 
 
-  database_user { 'edith@%': 
+  database_user { 'edith@localhost': 
     ensure => 'present', 
     password_hash => mysql_password('edith'), 
     provider => 'mysql', 
     require => [Database['edith'], Database['edith_test']], 
   } 
 
-  database_grant { 'edith@%/edith': 
+  database_grant { 'edith@localhost/edith': 
     privileges => 'all', 
     provider => 'mysql', 
-    require => Database_user['edith@%'], 
+    require => Database_user['edith@localhost'], 
     notify => Exec['collations-edith'],
   }   
 
-  database_grant { 'edith@%/edith_test': 
+  database_grant { 'edith@localhost/edith_test': 
     privileges => 'all', 
     provider => 'mysql', 
-    require => Database_user['edith@%'], 
+    require => Database_user['edith@localhost'], 
     notify => Exec['collations-edith_test'],
   }   
 
   exec { "collations-edith":
     command => '/usr/bin/mysql edith -e "ALTER DATABASE edith COLLATE utf8_swedish_ci;"',
     logoutput => true,
-    require => Database_grant['edith@%/edith'],
+    require => Database_grant['edith@localhost/edith'],
   }
 
   exec { "collations-edith_test":
     command => '/usr/bin/mysql edith_test -e "ALTER DATABASE edith_test COLLATE utf8_swedish_ci;"',
     logoutput => true,
-    require => Database_grant['edith@%/edith_test'],
+    require => Database_grant['edith@localhost/edith_test'],
   }
 }
