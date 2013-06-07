04ccc2fcfc177e9f00afdc1ae456a10e5ede3f71
Vesa Marttila <vtmarttila@gmail.com>
2013-06-07 13:37:12 +0300

    Log missing localization keys in JS console

--- src/main/webapp/js/localize.js
+++ src/main/webapp/js/localize.js
@@ -4,6 +4,9 @@ define(['jquery', 'handlebars', 'json!/api/localizations', 'sprintf'],
     var localize = function(key) {
       var args = Array.prototype.slice.call(arguments);
       args[0] = localizations[key];
+      if (!args[0] && console) {
+        console.log('Localization for key: ' + key + ' not found');
+      }
       return args[0] ? sprintf.apply(this, args) : key;      
     };
   
