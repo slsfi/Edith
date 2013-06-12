7228b29787090803be8517ff7f8d8dfc082f30c0
Vesa Marttila <vtmarttila@gmail.com>
2013-06-12 16:07:38 +0300

    Fix docnote delete

--- src/main/webapp/js/views/workspace/annotator/note-edit.js
+++ src/main/webapp/js/views/workspace/annotator/note-edit.js
@@ -159,7 +159,7 @@ define(['jquery', 'underscore', 'backbone', 'vent', 'handlebars', 'localize', 's
                      dataType: 'json',
                      contentType: "application/json; charset=utf-8",
                      success: function(data) {
-                       vent.trigger('annotation:change', self.documentNote.document);
+                       vent.trigger('annotation:change', self.documentNote.document.id);
                        // TODO: Note's DocumentNote count?
 //                       vent.trigger('note:change', self.note.id);
                        vent.trigger('document-note:change', data);
