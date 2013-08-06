c0ae5200efb400bfec487e3e2c7f52978b413b10
Timo Westkämper <timo.westkamper@mysema.com>
2013-08-06 11:08:35 +0300

    Add logging to filters

--- src/main/java/com/mysema/edith/guice/CharsetResponseFilter.java
+++ src/main/java/com/mysema/edith/guice/CharsetResponseFilter.java
@@ -1,14 +1,21 @@
 package com.mysema.edith.guice;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.MediaType;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.sun.jersey.spi.container.ContainerRequest;
 import com.sun.jersey.spi.container.ContainerResponse;
 import com.sun.jersey.spi.container.ContainerResponseFilter;
 
 public class CharsetResponseFilter implements ContainerResponseFilter {
+	
+	private static final Logger logger = LoggerFactory.getLogger(CharsetResponseFilter.class);
 
     public ContainerResponse filter(ContainerRequest request, ContainerResponse response) {
+    	logger.debug("charsetResponseFilter.doFilter {}", ((HttpServletRequest)request).getRequestURI());
         MediaType contentType = response.getMediaType();
         if (contentType != null) {
             response.getHttpHeaders().putSingle("Content-Type", contentType.toString() + ";charset=UTF-8");    
--- src/main/java/com/mysema/edith/guice/NoCacheFilter.java
+++ src/main/java/com/mysema/edith/guice/NoCacheFilter.java
@@ -6,15 +6,21 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.shiro.web.servlet.AbstractFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class NoCacheFilter extends AbstractFilter {
 
+	private static final Logger logger = LoggerFactory.getLogger(NoCacheFilter.class);
+	
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
+    	logger.debug("noCacheFilter.doFilter {}", ((HttpServletRequest)request).getRequestURI());
         HttpServletResponse res = (HttpServletResponse) response;
         res.setHeader("Cache-Control", "max-age=0");
         chain.doFilter(request, response);
