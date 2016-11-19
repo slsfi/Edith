ef32b1b3f8324d0714a856f226f89b732b7ac715
Mika Kivimäki <mika.kivimaki@mysema.com>
2016-11-19 19:13:46 +0200

    Update components and remove initial status by incremental SQL

---
+++ src/main/sql/incremental/010_remove_initial_status.sql
@@ -0,0 +1,6 @@
+UPDATE note SET status = 'DRAFT' WHERE status ='INITIAL';
+
+ALTER TABLE note ADD new_status_column ENUM('DRAFT', 'FINISHED') CHARACTER SET utf8 COLLATE utf8_swedish_ci DEFAULT NULL ;
+UPDATE note SET new_status_column = status;
+ALTER TABLE note DROP status;
+ALTER TABLE note CHANGE new_status_column status ENUM('DRAFT', 'FINISHED') CHARACTER SET utf8 COLLATE utf8_swedish_ci DEFAULT NULL ;
